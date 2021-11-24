63d7e6ffaa890ad03ee2eae5b35a008763f1040b
DanielMa <mawenliang000@gmail.com>
2021-11-24 18:01:35 +0800

    Add cards and conditional rendering

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,10 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import {Card, CardHeader, Text} from "@ui5/webcomponents-react"
 import {spacing} from "@ui5/webcomponents-react-base"
+import { BarChart, LineChart } from "@ui5/webcomponents-react-charts";
+
+
+const dataset = [
+  {
+    month: "January",
+    data: 65
+  },
+  {
+    month: "February",
+    data: 59
+  },
+  {
+    month: "March",
+    data: 80
+  },
+  {
+    month: "April",
+    data: 81
+  },
+  {
+    month: "May",
+    data: 56
+  },
+  {
+    month: "June",
+    data: 55
+  },
+  {
+    month: "July",
+    data: 40
+  }
+];
 
 export function MyApp() {
+  const [toggleCharts, setToggleCharts] = useState('lineChart')
+
   const handleHeaderClick = () => {
-    alert("Header clicked");
+     (toggleCharts === 'lineChart') ? setToggleCharts("barChart") : setToggleCharts("lineChart")
   }
   return (
   <div>
@@ -19,6 +54,19 @@ export function MyApp() {
       <Text style={spacing.sapUiContentPadding}>
         This is the content area of the Card
       </Text>
+      {toggleCharts === "lineChart" ? (
+      <LineChart 
+        dimensions={[{accessor: "month"}]} 
+        measures={[{accessor: "data", label: "Stock Price"}]} 
+        dataset={dataset}
+      />
+      ) : (
+      <BarChart
+        dimensions={[{accessor: "month"}]}
+        measures={[{accessor: "data", label: "Stock Price"}]}
+        dataset={dataset}
+       />
+      )}
     </Card>
   </div>
   );
