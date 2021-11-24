f66eafc1ebb998a8b700b2062810d9644a1cf9f7
DanielMa <mawenliang000@gmail.com>
2021-11-25 06:07:41 +0800

    Add Loading indicator and dynamic header text

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,7 +1,10 @@
 import React, {useState} from "react";
-import {Card, CardHeader, Text} from "@ui5/webcomponents-react"
+import {Card, CardHeader, Text, Icon} from "@ui5/webcomponents-react"
 import {spacing} from "@ui5/webcomponents-react-base"
 import { BarChart, LineChart } from "@ui5/webcomponents-react-charts";
+import '@ui5/webcomponents-icons/dist/line-chart.js';
+import '@ui5/webcomponents-icons/dist/horizontal-bar-chart.js';
+
 
 
 const dataset = [
@@ -37,34 +40,56 @@ const dataset = [
 
 export function MyApp() {
   const [toggleCharts, setToggleCharts] = useState('lineChart')
+  const [loading, setLoading] = useState(false)
 
   const handleHeaderClick = () => {
-     (toggleCharts === 'lineChart') ? setToggleCharts("barChart") : setToggleCharts("lineChart")
+     if (toggleCharts === 'lineChart'){
+       setLoading(true)
+       setTimeout(() => {
+         setLoading(false)
+         setToggleCharts("barChart") 
+       }, 1000)
+     }else
+     {
+       setLoading(true)
+       setTimeout(() => {
+          setLoading(false)
+          setToggleCharts("lineChart")
+        }, 1000)
+     }
   }
+
+  const contentTitle = toggleCharts === 'lineChart' ? 'Line Chart' : 'Bar Chart'
+  const switchToChart = toggleCharts === 'lineChart' ? 'Bar Chart' : 'Line Chart'
+
   return (
   <div>
     <Card 
       header={
         <CardHeader 
-          titleText="Card" 
+          titleText="Stock Prices"
+          subtitleText={`Click here to switch to ${switchToChart}`}
           interactive 
           onClick={handleHeaderClick}
+          avatar={<Icon name={toggleCharts === "lineChart" ? "line-chart" : "horizontal-bar-chart"} />}
          />} 
           style={{width: "300px"}} >
       <Text style={spacing.sapUiContentPadding}>
-        This is the content area of the Card
+        {contentTitle}
       </Text>
       {toggleCharts === "lineChart" ? (
       <LineChart 
         dimensions={[{accessor: "month"}]} 
         measures={[{accessor: "data", label: "Stock Price"}]} 
         dataset={dataset}
+        loading={loading}
       />
       ) : (
       <BarChart
         dimensions={[{accessor: "month"}]}
         measures={[{accessor: "data", label: "Stock Price"}]}
         dataset={dataset}
+        loading={loading}
        />
       )}
     </Card>
